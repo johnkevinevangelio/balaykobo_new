ef0643fd0be9e2aeb0bd1fde74b85b6800345893
John Kevin Evangelio <evangeliojohnkevin@gmail.com>
2024-03-20 11:27:10 +0800

    fix: style not applied

---
+++ bookStudio.php
@@ -0,0 +1,137 @@
+
+<style>
+    .bookStudio {
+        background-image: url('https://balaykobo.com/wp-content/uploads/2024/01/studio_header_bg-scaled.jpg');
+        background-position: center;
+        background-size: cover;
+        height: 100vh;
+    } 
+    .bookStudioBox {
+        background-color: rgba(255, 255, 255, 0.80);
+        height: 85%;
+        width: 40%;
+        padding: 30px 50px 50px 50px;
+        margin-top: 100px;
+    }
+    .bookStudioTitle {
+        color: #222;
+        font-family: Spectral;
+        font-size: 36px;
+        font-style: normal;
+        font-weight: 400;
+    }
+    .bookStudioDesc {
+        color: #3E352C;
+        font-family: Oxygen Mono;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+    }
+    .bookStudioBox2 {
+        padding: 30px;
+        margin-top: 30px;
+        background: #FFF;
+    }
+    .bookStudioBox2Content {
+        font-family: Oxygen Mono;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+    }
+    @media screen and (max-width:1200px) {
+            
+        .bookStudioBox {
+            background-color: rgba(255, 255, 255, 0.80);
+            height: 85%;
+            /* width: calc(100% - 30%); */
+            width: 100%;
+        }
+        .bookStudioTitle {
+            color: #222;
+            font-family: Spectral;
+            font-size: 28px;
+            font-style: normal;
+            font-weight: 400;
+        }
+        .bookStudioDesc {
+            color: #3E352C;
+            font-family: Oxygen Mono;
+            font-size: 12px;
+            font-style: normal;
+            font-weight: 400;
+        }
+    }
+    @media screen and (max-width:800px) {
+            
+        .bookStudioBox {
+            background-color: rgba(255, 255, 255, 0.80);
+            height: 90%;
+            padding: 30px;
+            /* width: calc(100% - 30%); */
+            width: 100%;
+        }
+        .bookStudioTitle {
+            color: #222;
+            font-family: Spectral;
+            font-size: 18px;
+            font-style: normal;
+            font-weight: 400;
+        }
+        .bookStudioDesc {
+            color: #3E352C;
+            font-family: Oxygen Mono;
+            font-size: 12px;
+            font-style: normal;
+            font-weight: 400;
+        }
+        .bookStudioBox2Content {
+            font-family: Oxygen Mono;
+            font-size: 12px;
+            font-style: normal;
+            font-weight: 400;
+        }
+    }
+    @media screen and (max-width:400px) {
+            
+        .bookStudioBox {
+            background-color: rgba(255, 255, 255, 0.80);
+            height: 450px;
+            padding: 0px;
+            /* width: calc(100% - 20%); */
+            width: 100%;
+        }
+        .bookStudioBox2Content {
+            font-family: Oxygen Mono;
+            font-size: 12px;
+            font-style: normal;
+            font-weight: 400;
+            width: 100%
+        }
+        iframe[src*="google.com/forms"] {
+            width: 100% !important;
+            height: 800px;
+        }
+    }
+</style>
+<div class="bookStudioBox">
+    <div class="bookStudioBody">
+        <div class="bookStudioTitle">
+            Book a Studio
+        </div>
+        <div class="bookStudioDesc">
+            Kindly fillout the following
+        </div>
+        <div class="bookStudioBox2 d-flex-justify-content-center">
+            <div class="bookStudioBox2Content">
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdkvViNTUpHw0be4m77B-BQfqRob-WEtw-kec1GIskTLQoXTQ/viewform?embedded=true"
+                    width="100%"
+                    height="450"
+                    frameborder="0"
+                    marginwidth="0"
+                    marginheight="0">
+                    Loading…
+                </iframe>
+            </div>
+        </div>
+    </div>
+</div>
